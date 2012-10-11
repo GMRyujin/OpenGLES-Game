ca6ceb2027f638b2a969a44b6a01d2514bd948ce
GMRyujin <GmRyuj@gmail.com>
2012-10-12 01:11:13 +0900

    Version 0.02
    Add the Test Version

--- Entry.cpp
+++ Entry.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
 
+/* This is OpenGL OpenSource Project Creating the Game */
 int main()
 {
-	cout << "Hellow Creating OpenGLES Game";
+	cout << "Hellow Creating OpenGLES Game" << endl;
+	cout << "Test Version" << endl;
 
 	return 0;
 }
